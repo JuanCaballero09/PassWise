72819070b042b863560708c9b3adff97a602a730
JuanCaballero09 <juanes0921200@gmail.com>
2025-08-01 14:19:19 -0500

    update

--- passwise_backend/app/controllers/api/v1/usuarios_controller.rb
+++ passwise_backend/app/controllers/api/v1/usuarios_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::UsuariosController < ApplicationController 
+  def index
+    @usuarios = Usuario.all.order(:id)
+    render json: @usuarios
+  end
 end
--- passwise_backend/config/routes.rb
+++ passwise_backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :usuarios, only: [ :create ]
+      resources :usuarios, only: [ :index, :create ]
       post 'login', to: 'authenticacion#create'
       resources :contrasenas
     end
